2f9c9441a321708336336faaa9f7775022b469ff
Kevin Kolz <kckolz@gmail.com>
2015-08-29 10:41:25 -0400

    Fixing log file path

--- pi-fi-api/controllers/deviceController.js
+++ pi-fi-api/controllers/deviceController.js
@@ -3,12 +3,14 @@
 // Load required packages
 var responseUtil = require('../util/responseUtil');
 var fs = require('fs');
+var path = require('path');
+var appDir = path.dirname(require.main.filename);
 
 var DeviceController = {
 
   log: function(req, res) {
     var log = req.body.deviceId + ',' + req.body.time + "\n";
-    fs.appendFile('deviceLog.csv', log, function (err) {
+    fs.appendFile(appDir+'/deviceLog.csv', log, function (err) {
       if(err) {
         return responseUtil.handleInternalError(err, "Failed to save log");
       } else {
