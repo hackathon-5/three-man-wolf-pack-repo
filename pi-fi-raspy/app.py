8deb558234b31316a5d61a089bd838baddf0b91c
Ryan Moore <rmoore4146@gmail.com>
2015-08-29 07:26:08 -0400

    kevin is tupid

--- pi-fi-raspy/app.py
+++ pi-fi-raspy/app.py
@@ -20,7 +20,7 @@ class PifiApp:
 
     visitedAddresses = []
     sortedTuples = sorted(addrRssiTuple, key=lambda addrRssi: addrRssi[1]) 
-    for nearestDevice in sortedTuples:
+    for nearestDevice in reversed(sortedTuples):
       if nearestDevice[1] > self.proximity and nearestDevice[0] not in visitedAddresses:
         visitedAddresses.append(nearestDevice[0])
         print(nearestDevice[0] + ' attempting device.')    
