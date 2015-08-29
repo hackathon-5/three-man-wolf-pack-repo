ee8d4399e3fd1cf2df41ff68bc0090d5884ab99a
Ryan Moore <rmoore4146@gmail.com>
2015-08-29 05:38:47 -0400

    android connects. finally.

--- pi-fi-android/app/src/main/java/pack/wolf/com/pifi/fragment/MainFragment.java
+++ pi-fi-android/app/src/main/java/pack/wolf/com/pifi/fragment/MainFragment.java
@@ -1,7 +1,6 @@
 package pack.wolf.com.pifi.fragment;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.InflateException;
@@ -10,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import pack.wolf.com.pifi.R;
-import pack.wolf.com.pifi.service.BluetoothService;
 
 public class MainFragment extends Fragment {
 
